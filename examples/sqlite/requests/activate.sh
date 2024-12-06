55f63db701251f9a52eae7e37f48a442a11e3ae5
Lut99 <mullertim99@gmail.com>
2024-12-06 17:55:21 +0100

    Fixed bug with example not using `ActivateRequest`

--- examples/sqlite/requests/activate.sh
+++ examples/sqlite/requests/activate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -v localhost:8080/v2/policies/active -X PUT -H 'Content-Type: application/json' -d '1'
+curl -v localhost:8080/v2/policies/active -X PUT -H 'Content-Type: application/json' -d '{ "version": 1 }'
